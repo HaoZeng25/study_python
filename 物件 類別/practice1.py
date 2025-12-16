4f0b7237cffa95279b69618a952e5689dda0ed9a
HaoZeng025 <hao.zeng@novakon.com.tw>
2025-12-16 18:12:15 +0800

    practice1

--- 物件 類別/practice1.py
+++ 物件 類別/practice1.py
@@ -4,3 +4,18 @@
 # 如未輸入則預設為0分(也就是缺考)。
 # 阿明的成績分別為：數學55/英文70/物理55
 # 小美的成績分別為：數學90/英文88/物理100
+
+class Student():
+    def __init__(self, name, subject=0):
+        self.name = name
+        self.subject = subject
+
+    def readyName(self):
+        print('聽清楚了，我的名字是' + self.name + '!!!')
+
+    # score = dict({'subject':'score'})
+    score = dict({'{subject}':'{score}'})
+
+# 呼叫時同時給予成績(注意要對應科目，程式才會知道)，成績若複數個，可以用list
+ming = Student('小明') #小明是學生
+mei = Student('小美')
